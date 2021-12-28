7d759bccec4c03bc1792cfc871859aa68b96f18c
Abhishek Jaiswal <abhishek_jaiswal@sislinfotech.com>
2021-12-28 11:34:06 +0530

    added rest controller and jsonwebtoken

---
+++ application/libraries/Jsonwebtokens.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use \Firebase\JWT\JWT;
+
+class Jsonwebtokens
+{
+
+    public function init()
+    {
+        // include APPPATH . 'third_party/firebase-jwt/JWT.php';
+    }
+
+    public function  generate_jswt($data = array(), $validity = 'P30D')
+    {
+        $CI = &get_instance();
+        $this->init();
+        $jwt = '';
+        $date = new DateTime();
+        $timeStart = $date->getTimestamp();
+        $date->add(new DateInterval($validity));
+        $timeEnd = $date->getTimestamp();
+        /*
+        * iss – Issuer application.
+        * iat – timestamp of token issuing.
+        * nbf – Timestamp of when the token should start being considered valid.
+        * exp – Timestamp of when the token should cease to be valid.
+        * data – Array of data
+        */
+        try {
+            $token = array(
+                "iss" => "aapkicare.com",
+                "aud" => "AapkiCARE +",
+                "iat" => $timeStart,
+                "nbf" => $timeStart,
+                "exp" => $timeEnd,
+                "data" => $data
+            );
+            $jwt = JWT::encode($token, "TokenByDeveloper", "HS256");
+        } catch (Exception $ex) {
+            log_message('error', $ex->getMessage());
+        } finally {
+            return $jwt;
+        }
+    }
+
+    public function  validate_jswt($token)
+    {
+        $this->init();
+        $jwt = null;
+        try {
+            $jwt = JWT::decode($token, "TokenByDeveloper", array("HS256"));
+        } catch (Exception $ex) {
+            log_message('error', $ex->getMessage());
+        } finally {
+            return $jwt;
+        }
+    }
+}
